47fd25429525733dde79f73f4120901c702b6d38
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-09-24 09:22:01 -0300

    Config of clienteController

--- projeto-full-ESA/backend/controllers/clienteController.js
+++ projeto-full-ESA/backend/controllers/clienteController.js
@@ -9,4 +9,16 @@ const listarClientes = (req, res) => {
 const adicionarCliente = (req, res) => {
     const {nome, email, empresa} = req.body;
     // ↑ extrai os dados vindos do front-end 
+
+    const novoCliente = {id: clientes.length + 1, nome, email, empresa};
+    // ↑ cria um novo cliente com id automático
+
+    clientes.push(novoCliente);
+    // ↑ adiciona o novo cliente ao "banco de dados" (array) 🤣
+
+    res.status(201).json(novoCliente);
+    // ↑ responde para o front-end confirmando que o novo cliente foi criado
 };
+
+module.exports = {listarClientes, adicionarCliente}
+// ↑ exportamos as funções para serem usadas em outras partes do sistema
